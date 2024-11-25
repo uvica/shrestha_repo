107582ded7a0559d763078e189c2e8aa1390728c
uvica <uvicaahts@gmail.com>
2024-11-25 08:23:56 +0545

    Add my file shrestha

---
+++ shrestha/Models/Student/Class.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace shrestha.Models.Student
+{
+    public class Class
+    {
+
+        
+            public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string phone { get; set; }
+        public DataType EnrollmentDate {  get; set; }
+        public string Address { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+
+
+    }
+}
